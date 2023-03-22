f00bbbee2637f53a9d5f29226cf8a0c4069470c9
karen gevorgyan <karen-gev.22@mail.ru>
2023-03-22 14:48:27 +0300

    First Commit

---
+++ src/main/java/pro/sky/database1/DataBase1Application.java
@@ -0,0 +1,13 @@
+package pro.sky.database1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataBase1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataBase1Application.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/database1/DataBase1ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.database1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataBase1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
